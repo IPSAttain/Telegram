607d3e442fee7a8c8a1e0266df2c6f0033eda5fd
Heimgeist <mail@heimgeist.net>
2016-08-18 07:18:18 +0000

    add new property HTML for parse_mode switching

--- TelegramMessenger/module.php
+++ TelegramMessenger/module.php
@@ -3,13 +3,13 @@
     class TelegramMessenger extends IPSModule {
  
         public function __construct($InstanceID) {
-            // Diese Zeile nicht löschen
+            // Diese Zeile nicht lÃ¶schen
             parent::__construct($InstanceID);
 			
 			$this->RegisterTimer("GetUpdates", 15000, 'Telegram_GetUpdates($_IPS[\'TARGET\']);');
         }
  
-        // Überschreibt die interne IPS_Create($id) Funktion
+        // Ãœberschreibt die interne IPS_Create($id) Funktion
         public function Create() {
             parent::Create();                       
 			$this->RegisterPropertyString("BotID", "123456789:JHJ56HJJHJ78778JKLKJKLJ8798JHJahjhw");
@@ -17,36 +17,37 @@
 			$this->RegisterPropertyBoolean("FetchIncoming", true);
 			$this->RegisterPropertyBoolean("ProcessIncoming", false);
 			$this->RegisterPropertyInteger ("ProcessIncomingSkript", 0);
+			$this->RegisterPropertyBoolean("HTML", false);
 		
 		}
 		
 		
-        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+        // Ãœberschreibt die intere IPS_ApplyChanges($id) Funktion
         public function ApplyChanges() {
-            // Diese Zeile nicht löschen
+            // Diese Zeile nicht lÃ¶schen
             parent::ApplyChanges();
 
         }
  
         /**
-        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
-        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+        * Die folgenden Funktionen stehen automatisch zur VerfÃ¼gung, wenn das Modul Ã¼ber die "Module Control" eingefÃ¼gt wurden.
+        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur VerfÃ¼gung gestellt:
         *
         * ABC_MeineErsteEigeneFunktion($id);
         *
         */
 		
-		public function SendTextToAll($text, $format='Markdown') {
+		public function SendTextToAll($text) {
 			$recips = explode(",",$this->ReadPropertyString("Recipients"));
 			foreach($recips as $r) {
-				$this->SendText($text, $r, $format);
+				$this->SendText($text, $r);
 			}			
 		}
 		
-		public function SendText($text, $userid, $format='Markdown') {
+		public function SendText($text, $userid) {
 			include_once(__DIR__ . "/Telegram.php");
 			$frmt = "Markdown";
-			if ($format == "HTML") {
+			if ($this->ReadPropertyBoolean("HTML") == true) {
 				$frmt = "HTML";
 			}
 			$telegram = new Telegram($this->ReadPropertyString("BotID"));
@@ -113,7 +114,7 @@
 					$chat_id = $telegram->ChatID();
 					$date = $telegram->Date();
 					IPS_LogMessage("Telegram", "Update von " . $chat_id . " -> " . $text . " / " . $date . " / " . print_r($telegram,true));
-					// Verarbeiten von Nachrichten (aber nur wenn aktiviert und Nachricht nicht älter als 1 Minute);
+					// Verarbeiten von Nachrichten (aber nur wenn aktiviert und Nachricht nicht Ã¤lter als 1 Minute);
 					if ($this->ReadPropertyBoolean("ProcessIncoming") && (time() - $date) < 60) {
 						// Ist der User bekannt?
 						$recips = explode(",",$this->ReadPropertyString("Recipients"));
