c76e94699112d1285781ade763b8010ed12f1dab
Jens Cramer <jmc@jmc.bz>
2017-03-06 14:38:34 +0100

    Ticket #1: RegisterTimer in Create-Methode verschoben Ticket von Paresy, der RegisterTimer muss vom __construct in Create() verschoben werden um die Kompatibilität mit IPS 4.2 sicher zu stellen.

--- TelegramMessenger/module.php
+++ TelegramMessenger/module.php
@@ -3,13 +3,11 @@
     class TelegramMessenger extends IPSModule {
  
         public function __construct($InstanceID) {
-            // Diese Zeile nicht lÃ¶schen
+            // Diese Zeile nicht löschen
             parent::__construct($InstanceID);
-			
-			$this->RegisterTimer("GetUpdates", 15000, 'Telegram_GetUpdates($_IPS[\'TARGET\']);');
         }
  
-        // Ãœberschreibt die interne IPS_Create($id) Funktion
+        // Überschreibt die interne IPS_Create($id) Funktion
         public function Create() {
             parent::Create();                       
 			$this->RegisterPropertyString("BotID", "123456789:JHJ56HJJHJ78778JKLKJKLJ8798JHJahjhw");
@@ -17,21 +15,20 @@
 			$this->RegisterPropertyBoolean("FetchIncoming", true);
 			$this->RegisterPropertyBoolean("ProcessIncoming", false);
 			$this->RegisterPropertyInteger ("ProcessIncomingSkript", 0);
-			$this->RegisterPropertyBoolean("HTML", false);
-		
+			$this->RegisterTimer("GetUpdates", 15000, 'Telegram_GetUpdates($_IPS[\'TARGET\']);');
 		}
 		
 		
-        // Ãœberschreibt die intere IPS_ApplyChanges($id) Funktion
+        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
         public function ApplyChanges() {
-            // Diese Zeile nicht lÃ¶schen
+            // Diese Zeile nicht löschen
             parent::ApplyChanges();
 
         }
  
         /**
-        * Die folgenden Funktionen stehen automatisch zur VerfÃ¼gung, wenn das Modul Ã¼ber die "Module Control" eingefÃ¼gt wurden.
-        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur VerfÃ¼gung gestellt:
+        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
         *
         * ABC_MeineErsteEigeneFunktion($id);
         *
@@ -46,12 +43,8 @@
 		
 		public function SendText($text, $userid) {
 			include_once(__DIR__ . "/Telegram.php");
-			$frmt = "Markdown";
-			if ($this->ReadPropertyBoolean("HTML") == true) {
-				$frmt = "HTML";
-			}
 			$telegram = new Telegram($this->ReadPropertyString("BotID"));
-			$content = array('chat_id' => $userid, 'text' => $text, 'parse_mode' => $frmt);
+			$content = array('chat_id' => $userid, 'text' => $text, 'parse_mode' => "Markdown");
 			$telegram->sendMessage($content);
 		}
 		
@@ -114,7 +107,7 @@
 					$chat_id = $telegram->ChatID();
 					$date = $telegram->Date();
 					IPS_LogMessage("Telegram", "Update von " . $chat_id . " -> " . $text . " / " . $date . " / " . print_r($telegram,true));
-					// Verarbeiten von Nachrichten (aber nur wenn aktiviert und Nachricht nicht Ã¤lter als 1 Minute);
+					// Verarbeiten von Nachrichten (aber nur wenn aktiviert und Nachricht nicht älter als 1 Minute);
 					if ($this->ReadPropertyBoolean("ProcessIncoming") && (time() - $date) < 60) {
 						// Ist der User bekannt?
 						$recips = explode(",",$this->ReadPropertyString("Recipients"));
