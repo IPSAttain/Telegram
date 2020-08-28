d0c1ec2e99a80b6737e732832f6564f39317338e
IPSAttain <christianwagner1@yahoo.de>
2020-08-28 17:01:33 +0200

    Name added

--- TelegramMessenger/module.php
+++ TelegramMessenger/module.php
@@ -3,11 +3,11 @@
     class TelegramMessenger extends IPSModule {
  
         public function __construct($InstanceID) {
-            // Diese Zeile nicht löschen
+            // Diese Zeile nicht lï¿½schen
             parent::__construct($InstanceID);
         }
  
-        // Überschreibt die interne IPS_Create($id) Funktion
+        // ï¿½berschreibt die interne IPS_Create($id) Funktion
         public function Create() {
             parent::Create();                       
 			$this->RegisterPropertyString("BotID", "123456789:JHJ56HJJHJ78778JKLKJKLJ8798JHJahjhw");
@@ -15,21 +15,22 @@
 			$this->RegisterPropertyBoolean("FetchIncoming", true);
 			$this->RegisterPropertyBoolean("ProcessIncoming", false);
 			$this->RegisterPropertyInteger ("ProcessIncomingSkript", 0);
+			$this->RegisterPropertyInteger ("DeniedIncomingSkript", 0);
 			$this->RegisterPropertyBoolean("HTML", false);
 			$this->RegisterTimer("GetUpdates", 15000, 'Telegram_GetUpdates($_IPS[\'TARGET\']);');
 		}
 		
 		
-        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+        // ï¿½berschreibt die intere IPS_ApplyChanges($id) Funktion
         public function ApplyChanges() {
-            // Diese Zeile nicht löschen
+            // Diese Zeile nicht lï¿½schen
             parent::ApplyChanges();
 
         }
  
         /**
-        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
-        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+        * Die folgenden Funktionen stehen automatisch zur Verfï¿½gung, wenn das Modul ï¿½ber die "Module Control" eingefï¿½gt wurden.
+        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfï¿½gung gestellt:
         *
         * ABC_MeineErsteEigeneFunktion($id);
         *
@@ -117,19 +118,28 @@
 					$text = $telegram->Text();
 					$chat_id = $telegram->ChatID();
 					$date = $telegram->Date();
+					$first_name = $telegram->FirstName();
+					$last_name = $telegram->LastName();
 					IPS_LogMessage("Telegram", "Update von " . $chat_id . " -> " . $text . " / " . $date . " / " . print_r($telegram,true));
-					// Verarbeiten von Nachrichten (aber nur wenn aktiviert und Nachricht nicht älter als 1 Minute);
+					// Verarbeiten von Nachrichten (aber nur wenn aktiviert und Nachricht nicht ï¿½lter als 1 Minute);
 					if ($this->ReadPropertyBoolean("ProcessIncoming") && (time() - $date) < 60) {
 						// Ist der User bekannt?
 						$recips = explode(",",$this->ReadPropertyString("Recipients"));
+						$GuarantAccess = false;
 						foreach($recips as $r) {
 							if ($r == $chat_id) {
 								IPS_RunScriptEx(
 									$this->ReadPropertyInteger("ProcessIncomingSkript"),
-									array("SENDER" => "Telegram", "INSTANCE" => $this->InstanceID, "CHAT" => $chat_id, "VALUE" => $text)
+									array("SENDER" => "Telegram", "INSTANCE" => $this->InstanceID, "CHAT" => $chat_id, "VALUE" => $text, "LASTNAME" => $last_name, "FIRSTNAME" => $first_name)
 								);
+								$GuarantAccess = true;
 								break;
 							}
+						}
+						if (!$GuarantAccess) {
+							IPS_RunScriptEx(
+								$this->ReadPropertyInteger("DeniedIncomingSkript"),
+								array("SENDER" => "Telegram", "INSTANCE" => $this->InstanceID, "CHAT" => $chat_id, "VALUE" => $text, "LASTNAME" => $last_name, "FIRSTNAME" => $first_name)
 						}						
 					}
 				}
