19ea59058eb3b3f2dfcc9cf666e1d4152e1a96f0
IPSAttain <christianwagner1@yahoo.de>
2020-10-30 18:18:35 +0100

    Auswahlkeyboard

--- TelegramMessenger/module.php
+++ TelegramMessenger/module.php
@@ -46,7 +46,18 @@
 			$content = array('chat_id' => $userid, 'text' => $text, 'parse_mode' => $frmt);
 			return $telegram->sendMessage($content);
 		}
-		
+
+		public function SendTextKey(string $text, string $userid, string $markup) {
+			include_once(__DIR__ . "/../libs/Telegram.php");
+			$frmt = "Markdown";
+			if ($this->ReadPropertyBoolean("HTML") == true) {
+					$frmt = "HTML";
+			}
+			$telegram = new Telegram($this->ReadPropertyString("BotID"));
+			$content = array('chat_id' => $userid, 'text' => $text, 'parse_mode' => $frmt,'reply_markup' => $markup);
+			return $telegram->sendMessage($content);
+		}
+
 		public function SendImage(string $text, string $image_path, string $userid) {
 			include_once(__DIR__ . "/../libs/Telegram.php");
 			$telegram = new Telegram($this->ReadPropertyString("BotID"));
@@ -163,6 +174,6 @@
 					}
 				}
 			}
-		}
-    }
+		}  
+	}
 ?>
